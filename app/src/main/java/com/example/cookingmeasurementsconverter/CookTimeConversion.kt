afba80bbae24a101559fadc507a98b59d6daafa8
deanno <deanno@oregonstate.edu>
2024-01-11 22:20:03 -0500

    added functionality to CookTimeConversion

--- app/src/main/java/com/example/cookingmeasurementsconverter/CookTimeConversion.kt
+++ app/src/main/java/com/example/cookingmeasurementsconverter/CookTimeConversion.kt
@@ -1,14 +1,20 @@
 package com.example.cookingmeasurementsconverter
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.w3c.dom.Text
+import kotlin.math.sqrt
 
 class CookTimeConversion : AppCompatActivity() {
+    private var isModifyingTime: Boolean? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.time_conversion)
@@ -22,28 +28,67 @@ class CookTimeConversion : AppCompatActivity() {
         val modifyTimeButton = findViewById<Button>(R.id.changeTime)
         val modifyTempButton = findViewById<Button>(R.id.changeTemp)
 
-        val desiredChangeTitle = findViewById<TextView>(R.id.desiredConversion)
+        var desiredChangeTitle = findViewById<TextView>(R.id.desiredConversion)
         val desiredInput = findViewById<EditText>(R.id.desiredConversionEditText)
-        val desiredUnits = findViewById<TextView>(R.id.desiredConversionUnits)
+        var desiredUnits = findViewById<TextView>(R.id.desiredConversionUnits)
+        desiredInput.visibility = View.INVISIBLE
+        desiredUnits.visibility = View.INVISIBLE
 
         val calculateButton = findViewById<Button>(R.id.calculateConversion)
+        calculateButton.visibility = View.INVISIBLE
 
-        val resultTitle = findViewById<TextView>(R.id.conversionResult)
-        val result = findViewById<TextView>(R.id.result)
-        val resultUnits = findViewById<TextView>(R.id.resultUnits)
+        var resultTitle = findViewById<TextView>(R.id.conversionResult)
+        var result = findViewById<TextView>(R.id.result)
+        var resultUnits = findViewById<TextView>(R.id.resultUnits)
+
+        modifyTimeButton.setOnClickListener {
+            isModifyingTime = true
+            desiredChangeTitle.text = "Desired Cook Time"
+            desiredInput.visibility = View.VISIBLE
+            desiredUnits.visibility = View.VISIBLE
+            calculateButton.visibility = View.VISIBLE
+            desiredUnits.text = originalTimeSpinner.selectedItem.toString()
+        }
+        modifyTempButton.setOnClickListener {
+            isModifyingTime = false
+            desiredChangeTitle.text = "Desired Cook Temperature"
+            desiredInput.visibility = View.VISIBLE
+            desiredUnits.visibility = View.VISIBLE
+            calculateButton.visibility = View.VISIBLE
+            desiredUnits.text = originalTempSpinner.selectedItem.toString()
+        }
+        calculateButton.setOnClickListener {
+            if (isModifyingTime == true) {
+                resultTitle.text = "NEW COOK TEMPERATURE"
+                resultUnits.text = originalTempSpinner.selectedItem.toString()
+                result.text = modifyCookTime(originalTempInput, originalTimeInput, desiredInput)
+            }
+            else if (isModifyingTime == false) {
+                resultTitle.text = "NEW COOK TIME"
+                resultUnits.text = originalTimeSpinner.selectedItem.toString()
+                result.text = modifyCookTemp(originalTempInput, originalTimeInput, desiredInput)
+            }
+        }
 
         val backButton = findViewById<Button>(R.id.backButton)
         val resetButton = findViewById<Button>(R.id.resetButton)
 
     }
 
-    private fun timeCalculator(givenTemp: Int, givenTime: Int, desiredTime: Int): Int {
-        val timeDiff = givenTime / desiredTime
-        return timeDiff * givenTemp
+    private fun modifyCookTime(givenTemp: EditText, givenTime: EditText, desiredTime: EditText): String {
+        Log.d("OriginalTemp", "givenTemp: ${givenTemp.text}")
+        Log.d("originalTime", "givenTime: ${givenTime.text}")
+        Log.d("desiredTime", "desiredTime: ${desiredTime.text}")
+        val timeDiff = givenTime.text.toString().toDouble() / desiredTime.text.toString().toDouble()
+        val result = timeDiff * givenTemp.text.toString().toDouble()
+        Log.d("timeDiff", "timeDiff: $timeDiff")
+        Log.d("result", "result: $result")
+        return result.toInt().toString()
     }
 
-    private fun tempCalculator(givenTemp: Int, givenTime: Int, desiredTemp: Int): Int {
-        val tempDiff = givenTemp / desiredTemp
-        return tempDiff * givenTime
+    private fun modifyCookTemp(givenTemp: EditText, givenTime: EditText, desiredTemp: EditText): String {
+        val tempDiff = givenTemp.text.toString().toDouble() / desiredTemp.text.toString().toDouble()
+        val result = tempDiff * givenTime.text.toString().toDouble()
+        return result.toInt().toString()
     }
 }
